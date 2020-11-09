c52e3b85a1c5a261874505a757e6f36f40e528dc
Pedro Leonel <49409393+Lioninsk@users.noreply.github.com>
2020-11-08 21:05:51 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,19 +4,21 @@
 
 import Cpu, CpuEstado, Memoria, Interface
 
-memoria = Memoria.Memoria()
-cpu = Cpu.Cpu(memoria)
-vet = [None]*4
-cpuEstado = CpuEstado.CpuEstadoT()
-instrucoes = Interface.leituraDoArquivo()
+memoria = Memoria.Memoria()#inicializa memoria
+cpu = Cpu.Cpu(memoria)#inicializa cpu passando acesso a memoria
+vet = [None]*4#inicializa vetor com 4 posicoes nulas
+cpuEstado = CpuEstado.CpuEstadoT()#inicializa cpu estado
+instrucoes = Interface.leituraDoArquivo()#leitura do arquivo
 
-Interface.cpu_estado_inicializa(cpuEstado)
-Interface.cpu_altera_estado(cpu, cpuEstado)
-Interface.cpu_altera_programa(cpu, instrucoes)
-Interface.cpu_altera_dados(cpu, vet)
+Interface.cpu_estado_inicializa(cpuEstado)#definição das variaveis iniciais cpuEstado
+Interface.cpu_altera_estado(cpu, cpuEstado)#definição das variaveis iniciais da cpu
+Interface.cpu_altera_programa(cpu, instrucoes)#passagem das instrucoes para memoria de programa
+Interface.cpu_altera_dados(cpu, vet)#passagem do vetor para memoria de dados
 
-while(Interface.cpu_interrupcao(cpu) == "normal"):
+while(Interface.cpu_interrupcao(cpu) == "normal"):#enquanto não houver interrupção executa
     Interface.executa(cpu)
 
 print(f"Cpu parou na instrucao {Interface.cpu_instrucao(cpu)}.")
 print(f"O valor de m[0] eh {cpu.getValorMemoriaDeDados(0)}")
+
+###fim parte 1
